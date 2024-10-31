da3eefcc775af6e93045738bb2a13910b80ec166
txrnqt <jaransas2008@gmail.com>
2024-10-31 11:52:54 -0500

    startied converting over unbuntu

---
+++ unbuntu/FirstStep.sh
@@ -0,0 +1,12 @@
+#Did you do the forensic questiosn
+
+#update
+    
+    sudo apt update
+
+#upgrade
+
+#not every image will need an upgrade if it dosent need a upgrade 
+#dont run the following command if it dosent need an upgrade
+    
+    sudo apt upgrade
---
+++ unbuntu/READ-ME.sh
@@ -0,0 +1,72 @@
+#always solve the forensic questions frist even before opening the read me 
+
+#you will see the words nano and pico a lot these are text editors such as 
+#note pad or vscode there just terminal based your able to use them interchangblely 
+
+#ex:
+
+    nano myScript.sh
+    pico myScript.sh
+
+#these will open the same fiie to create a file you can either use touch or 
+#directly use the text editor command. using the text editor command will 
+#automaticly put you inside of the “doc” to edit it 
+
+#ex:
+
+    touch example.sh
+    nano example.sh
+
+#or 
+
+    nano example.shh
+
+#hope fully if you are using this checklist you learned basic bash scripting 
+#like i asked you to do but i expectied you to not well because your you (YES YOU REED/MARWA) 
+#fortunetly i know how so i can quickly explain this for you event that i was unable to attend 
+#evey time you make a scipt you have to put the .sh file indicator at the end to show that is a 
+#script like this
+
+    nano myScript.sh
+
+
+#for this example we will be using the nano(pico) text editor when you open the file it will be 
+#blank like this but with simple commands at the bottom
+
+
+
+#the very first thing you do with scripts is to but #!/bin/bash like this
+
+#!/bin/bash
+
+
+#then you paste the script below after words you press control + O to save/write 
+#(i will be refering to this as write) then control + X to exit the script you will 
+#be back in there terminal now clear the terminal by typeing clear some of the scripts 
+#can place a wall of text and can be unread able to some 
+
+
+clear
+
+
+#then type bash then the name of your script
+
+
+bash myScript.sh
+
+
+#there you’ve ran and made your first script this is one of 2 methods the 
+#other one is to stright copy and paste into the terimanl this works better for 
+#shorter scripts i showed you this one first becuase more of the scipts will work 
+#better with it own file so it is easer to manage. 
+
+#YOU CANNOT USE SHORT CUTS YOU MUST RIGHT CLICK 
+
+#now you can open the read me take a photo of the athurized users and then follow the First Step doc
+#now use your brain here dont be stupid if the read me says dont upgrade then only use the update 
+#command and close out the upgrade pop ups fsrom here use the photo of the authorized users and follow the Check Users doc 
+#now dont be an idiot take a photo of the authoized apps and solve the image make sure that the authorized apps are fully 
+#up to date and if you some how mess up the image by using scripts that I HAVE NOT TESTED i will find you and murder you 
+#for runing my image have a fun time :D
+
+#and if you get to this point and cant figure out how to open the teminal its alt+control+T
